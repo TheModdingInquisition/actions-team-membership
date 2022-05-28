af0c319b8cf8a881a6fc6194f916aaad758589fe
Matyrobbrt <matyrobbrt@gmail.com>
2022-05-28 21:51:38 +0300

    Try to fix errors

--- src/teamChecker.js
+++ src/teamChecker.js
@@ -1,4 +1,4 @@
-import * as core from '@actions/core';
+const core = require('@actions/core');
 import { context, getOctokit } from '@actions/github';
 
 const query = `query($pg: String, $organization: String!, $userLogins: [String!], $username: String!)  {
