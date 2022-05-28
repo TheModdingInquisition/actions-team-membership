83a0fe2aa378fe1f7fc8d692f40a68a66213cfb8
Matyrobbrt <matyrobbrt@gmail.com>
2022-05-28 20:58:26 +0300

    Initial try

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+import checker from './teamChecker';
+
+run();
+
+async function run() {
+    const token = core.getInput('token') ? core.getInput('token') : process.env['GITHUB_TOKEN'];
+    const username = context.actor;
+    const team = context.getInput('team', {required: true});
+    const teams = await checker.getTeams(token, username);
+
+    core.setOutput('teams', teams);
+    const teamPresent = teams.some(te => te.toLocaleLowerCase() == team.toLocaleLowerCase());
+    core.setOutput('permitted', teamPresent);
+
+    if (core.getInput('comment') && !teamPresent) {
+        const comment = core.getInput('comment');
+        const issueNumber = context.payload.issue?.number;
+        if (comment.length > 0 && issueNumber != 0) {
+            const octokit = getOctokit(token);
+            const { owner, repo } = context.repo;
+            await octokit.rest.issues.createComment({
+                owner: owner,
+                repo: repo,
+                issue_number: issueNumber,
+                body: comment,
+              });
+        }
+    }
+
+    if (core.getInput('exit').toLocaleLowerCase() == 'true') {
+        core.setFailed('Not permitted.');
+    }
+}
---
+++ src/teamChecker.js
@@ -0,0 +1,44 @@
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+const query = `query($pg: String, $organization: String!, $userLogins: [String!], $username: String!)  {
+    user(login: $username) {
+        id
+    }
+    organization(login: $org) {
+      teams (first:1, userLogins: $userLogins, after: $pg) { 
+          nodes {
+            name
+        }
+        pageInfo {
+          hasNextPage1
+          endCursor
+        }        
+      }
+    }
+}`
+
+const getTeams = function(token, username) {
+    const octokit = getOctokit(token);
+    const org = !core.getInput('organization') ? context.repo : core.getInput('organization');
+
+    var teams = []
+
+    // Pagination
+    var pg = null
+    do {
+        const data = await octokit.graphql(query, {
+            "pg": pg,
+            "organization": org,
+            "userLogins": [username],
+            "username": username
+        })
+
+        teams = teams.concat(data.organization.teams.nodes.map(val => val.name))
+
+        cursor = data.organization.teams.pageInfo.endCursor
+    } while (data.organization.teams.pageInfo.hasNextPage)
+    return teams;
+}
+
+export { getTeams };
